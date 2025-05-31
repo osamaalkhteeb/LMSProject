4d4ab814c75e5e087f3c70cc993c763e20b1a6ba
osamaalkhatib <osama200181@gmail.com>
2025-06-01 00:19:56 +0300

    V1.3

--- try 2/server/utils/validate.js
+++ try 2/server/utils/validate.js
@@ -16,6 +16,11 @@ export const validateRequest = (schema) => (req, res, next) => {
 };
 
 export const schema = {
+  register: Joi.object({
+  name: Joi.string().min(3).max(100).required(),
+  email: Joi.string().email().required(),
+  password: Joi.string().min(6).required()
+}),
   login: Joi.object({
     email: Joi.string().email().required(),
     password: Joi.string().min(6).required(),
